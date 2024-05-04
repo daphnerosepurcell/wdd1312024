1389d4a1a75cbf49974f239ee9b14281e29433e2
purcelldaphnerose <purcelldaphnerose@gmail.com>
2024-05-03 19:22:33 -0600

    New main page

---
+++ Practice/dom-basics.js
@@ -0,0 +1,7 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://picsum.photos/200");
+newImage.setAttribute("alt", "Random Image from picsum.");
+document.body.appendChild(newImage);
